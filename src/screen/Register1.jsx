0cb347e5aaba2bf4644476249ccdfbca9df488a7
kalaiarasan27 <kalai73579@gmail.com>
2024-09-27 13:42:49 +0530

    Resend OTP commit

--- src/screen/Register1.jsx
+++ src/screen/Register1.jsx
@@ -39,7 +39,7 @@ const Register1 = () => {
   const [emailError, setEmailError] = useState("");
   const [error, setError] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
-  const [captchaValue, setCaptchaValue] = useState(null);
+  // const [captchaValue, setCaptchaValue] = useState(null);
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const [isPasswordVisible1, setIsPasswordVisible1] = useState(false);
   const navig = useNavigate();
@@ -283,10 +283,10 @@ const Register1 = () => {
   
     // If no validation errors, proceed with form submission
     if (Object.keys(formErrors).length === 0) {
-      if (!captchaValue) {
-        setError("Please complete the reCAPTCHA.");
-        return;
-      }
+      // if (!captchaValue) {
+      //   setError("Please complete the reCAPTCHA.");
+      //   return;
+      // }
   
       localStorage.removeItem("formData");
   
@@ -629,11 +629,11 @@ const Register1 = () => {
      
  
               <input type="hidden" name="role" value={formData.role} />
-              <ReCAPTCHA
+              {/* <ReCAPTCHA
                 sitekey="6Lfv4zQqAAAAAKAqWKH0hazJhWnjHmpH8WYMoiNp"
                 onChange={(value) => setCaptchaValue(value)}
                 className="mt-4"
-              />
+              /> */}
              
               <div className="terms mt-4">
                 <input
